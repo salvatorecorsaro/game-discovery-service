d93ff93a7f2eba78f3987087fc2cdc04cf3df04e
salva <salva@>
2021-10-07 23:31:12 +0200

    Initial commit

---
+++ src/test/java/com/example/gamediscoveryservice/GameDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gamediscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
